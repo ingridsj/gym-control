eff4171da6ed63b1d673a0cd9685c02b3b6fcca3
ingridsj <ingridsjalmeida@gmail.com>
2021-01-18 12:01:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require("./routes")
 
 const server = express()
 
-server.get("/", function(req, res){
-    return res.send ("Hi!")
+server.use(express.static('public'))
+server.use(routes)
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server
 })
 
 server.listen(5000, function(){

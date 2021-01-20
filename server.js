12d6007fc05f7a7a3beea2f4c68b9e66875e50fa
ingridsj <ingridsjalmeida@gmail.com>
2021-01-20 13:36:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require("./routes")
 
 const server = express()
 
+server.use(express.urlencoded({ extended: true})) //config para receber e ler os dados do front
 server.use(express.static('public')) //Arrumando a pasta public
 server.use(routes) //Criando as rotas
 
